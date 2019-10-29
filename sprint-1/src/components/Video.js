594ae6ef16f43733298f037997fead044c2713bd
kevin <chengyu0718@hotmail.com>
2019-10-29 13:12:03 -0700

    video update

--- sprint-1/src/components/Video.js
+++ sprint-1/src/components/Video.js
@@ -7,17 +7,20 @@ import Volume from '../assets/Icons/SVG/Icon-volume.svg';
 export default function Video() {
 	return (
 		<div className="vid">
-			<div>
-				<img id="play-button" src={Play} alt="play" />
-				<div>
-					<img src={FullScreen} />
-					<img src={Volume} />
-				</div>
-			</div>
-
 			<video className="vid-video">
 				<source src={Sample} type="video/mp4" />
 			</video>
+
+			<div className="vid-button">
+				<div className="video__play-box">
+					<img className="video-button" id="play-button" src={Play} alt="play" />
+				</div>
+				<input type="range" className="video-range" id="range-button" />
+				<div className="vid-right">
+					<img className="video-button" id="fullScreen-button" src={FullScreen} />
+					<img className="video-button" id="volume-button" src={Volume} />
+				</div>
+			</div>
 		</div>
 	);
 }
